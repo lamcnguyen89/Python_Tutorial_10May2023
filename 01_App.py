8b4d055cb1b93f1fbf69189baa81c7346ec6ce7f
Lam Nguyen <lamcnguyen89@gmail.com>
2023-05-10 03:39:31 -0400

    Going over Variable

--- 01_App.py
+++ 01_App.py
@@ -1,4 +1,34 @@
+# Initial App
 print("My heart is Broken")
 print("o----")
 print(" ||||")
 print('*' * 10)
+
+# Variables
+price = 10
+price = 20 # Integer
+rating = 4.9 # Integer
+name = "Lam" # String
+is_published = True # Boolean
+
+print("Variable: ", str(price), '\nRating: ', str(rating), "\nName: ", name)
+
+# Let's write a program for a hospital. 
+    # His name is John Smith
+    # Age = 20 Years old
+    # Is a New Patient
+
+name = 'John Smith'
+age = 20
+newPatient = True
+
+print('Patient Name', name, '/nPatient Age', age, '\nIs he a New Patient?', newPatient)
+
+
+# Inbuilt functions in python:
+
+name = input('What is your name? ')
+print('Hi ', name + "!")
+
+favoriteColor = input('What is your favorite color? ')
+print(name + " likes the color " + favoriteColor)
