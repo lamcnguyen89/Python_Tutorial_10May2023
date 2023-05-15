e34c5b85dfd1bb65ce394138495ec8acf1589d75
Lam Nguyen <lamcnguyen89@gmail.com>
2023-05-15 15:14:53 -0400

    Finished Intro Tutorial. Now starting on some projects.

---
+++ Python_Intro_Tutorials/33_Files_and_Directories.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+# Absolute path: Path Starting from root of hard disk
+    # c:\Program Files\Microsoft
+    # /usr/local/bin
+# Relative Path: path starting from current directory
+
+path = Path("ecommerce")
+print(path.exists()) # checks to see if path exists
+path = Path("Ecommerce01")
+
+if not path.exists():
+    path.mkdir() # Makes a new folder
+
+path.rmdir() # Removes a Directory
+
+
+path = Path()
+for file in path.glob('*.py'):
+    print(file) # Search all files in a directory
